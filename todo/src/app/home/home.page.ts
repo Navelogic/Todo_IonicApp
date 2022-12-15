1b827aef1229ed9d48cc85cc820efd13b1cc6a5a
Arian Weslley <weslleysantoszbd@gmail.com>
2022-12-15 15:23:32 -0300

    Finalização de Saudação

--- todo/src/app/home/home.page.ts
+++ todo/src/app/home/home.page.ts
@@ -7,8 +7,39 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  
   constructor(private alertController: AlertController) {}
+  saudacao: any;
+
+  data = new Date();
+  dia = String(this.data.getDate()).padStart(2, '0');
+  mes = String(this.data.getMonth() + 1).padStart(2, '0');
+  ano = this.data.getFullYear();
+  hora = this.data.getHours();
+
+  mensagemDeSaudacao(){
+    if(this.hora >= 12){
+      this.saudacao = "Boa tarde, ";
+    } else if(this.hora >= 18){
+      this.saudacao = "Boa noite, ";
+    } else if(this.hora <= 12){
+      this.saudacao = "Bom dia, ";
+    }
+    return this.saudacao;
+  };
+
+
+
+
+
+
+
+  
+  
+
+
+  
+  dataAtual: number = Date.now();
 
   async mostrarRotinaAdicao(){
     const alert = await this.alertController.create({
