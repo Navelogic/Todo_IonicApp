4f2302d76b81b2920d4cab609c6d238d8f9503d5
Arian Weslley <weslleysantoszbd@gmail.com>
2022-12-15 15:35:28 -0300

    Tratando dia da Semana

--- todo/src/app/home/home.page.ts
+++ todo/src/app/home/home.page.ts
@@ -10,12 +10,41 @@ export class HomePage {
   
   constructor(private alertController: AlertController) {}
   saudacao: any;
+  diaDaSemana: any;
 
   data = new Date();
   dia = String(this.data.getDate()).padStart(2, '0');
   mes = String(this.data.getMonth() + 1).padStart(2, '0');
   ano = this.data.getFullYear();
   hora = this.data.getHours();
+
+  dSemana = this.data.getDay();
+
+  diaSemanaTratado(){
+    if (this.dSemana == 0){
+      this.diaDaSemana = "Domingo";
+    }
+    else if (this.dSemana == 1){
+      this.diaDaSemana = "Segunda";
+    }
+    else if (this.dSemana == 2){
+      this.diaDaSemana = "Terça";
+    }
+    else if (this.dSemana == 3){
+      this.diaDaSemana = "Quarta";
+    }
+    else if (this.dSemana == 4){
+      this.diaDaSemana = "Quinta";
+    }
+    else if (this.dSemana == 5){
+      this.diaDaSemana = "Sexta";
+    }
+    else if (this.dSemana == 6){
+      this.diaDaSemana = "Sábado";
+    }
+    return this.diaDaSemana;
+  }
+
 
   mensagemDeSaudacao(){
     if(this.hora >= 12){
